95e2392df7f5f5f62e9bb4fba7928e8cca9ae8a4
Zakriaayaz <mianzakriya@hotmail.com>
2023-07-31 20:35:37 +0500

    abcd

--- App.js
+++ App.js
@@ -2,15 +2,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useEffect, useMemo, useReducer, useCallback } from "react";
 import { Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { auth_context } from "./contexts/AuthContext";
 import Onboarding from "./screens/Onboarding";
 import Profile from "./screens/Profile";
-import SplashScreen from "./screens/SplashScreen";
+import splash_screen from "./screens/SplashScreen";
 import Home from "./screens/Home";
 import { StatusBar } from "expo-status-bar";
-
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-import { AuthContext } from "./contexts/AuthContext";
 
 const Stack = createNativeStackNavigator();
 
@@ -52,7 +50,7 @@ export default function App({ navigation }) {
     })();
   }, []);
 
-  const authContext = useMemo(
+  const auth_context = useMemo(
     () => ({
       onboard: async data => {
         try {
@@ -88,11 +86,11 @@ export default function App({ navigation }) {
   );
 
   if (state.isLoading) {
-    return <SplashScreen />;
+    return <splash_screen />;
   }
 
   return (
-    <AuthContext.Provider value={authContext}>
+    <auth_context.Provider value={auth_context}>
       <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator>
@@ -114,6 +112,6 @@ export default function App({ navigation }) {
           )}
         </Stack.Navigator>
       </NavigationContainer>
-    </AuthContext.Provider>
+    </auth_context.Provider>
   );
 }
